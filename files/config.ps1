fcca90145e794e45334aa5df5f19bf91aa582c89
sivakaruppiah <44968911+sivakaruppiah@users.noreply.github.com>
2021-02-08 14:32:27 +0000

    Update config.ps1

--- files/config.ps1
+++ files/config.ps1
@@ -1,5 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.ps1'))
-hab license accept
-hab pkg install core/windows-service
-netsh advfirewall firewall add rule name="Habitat Butterfly API" dir=in action=allow protocol=TCP localport=9631
-Start-Service -Name Habitat
+Set-ExecutionPolicy Bypass -Scope Process -Force; netsh advfirewall firewall add rule name="Sql" dir=in action=allow protocol=TCP localport=1433
+Invoke-Sqlcmd -Query " USE [master];CREATE LOGIN [adminuser] WITH PASSWORD=N'adminuser', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF;ALTER SERVER ROLE [sysadmin] ADD MEMBER [adminuser]; "
+Invoke-Sqlcmd -Query " USE [master];EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2; "
+Restart-Service -Force MSSQLSERVER
